81ac5d4aa8afcf2ee9c76a53f8baed05fdc5e73c
Mati <matiasrmontenegro@gmail.com>
2024-03-01 17:14:14 +0100

    Initial Commit

---
+++ PuzzleTile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PuzzleTile
+//
+//  Created by Mati Montenegro on 01/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PuzzleTile/PuzzleTileApp.swift
@@ -0,0 +1,17 @@
+//
+//  PuzzleTileApp.swift
+//  PuzzleTile
+//
+//  Created by Mati Montenegro on 01/03/24.
+//
+
+import SwiftUI
+
+@main
+struct PuzzleTileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
